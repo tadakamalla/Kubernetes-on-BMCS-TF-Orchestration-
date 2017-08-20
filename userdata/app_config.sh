3396b770f330dd9ad0deedc4b5e58c20083775cb
Aravind Tadakamalla <tadakamalla@gmail.com>
2017-08-19 22:21:49 -0400

    added

--- userdata/app_config.sh
+++ userdata/app_config.sh
@@ -5,3 +5,4 @@ kubectl create -f /tmp/mynode-service.yaml
 kubectl create -f /tmp/redis-master.yaml
 kubectl create -f /tmp/redis-sentinel-service.yaml
 kubectl create -f /tmp/redis-controller.yaml
+
